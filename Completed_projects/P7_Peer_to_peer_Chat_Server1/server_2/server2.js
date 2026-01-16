2319405f35ce8f9dc08823eada93d0afdf2d6b2f
D-Shvedov <d.shvedov@asc.de>
2026-01-16 15:33:36 +0100

    Peer = server

---
+++ Completed_projects/P7_Peer_to_peer_Chat_Server1/server_2/server2.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const Gun = require("gun");
+const path = require("path");
+const http = require("http");
+
+const app = express();
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/sign.html"));
+});
+
+const server = http.createServer(app);
+
+Gun({
+  web: server,
+  file: path.join(__dirname, "radata")
+});
+
+server.listen(8766, "0.0.0.0", () => {
+  console.log("GUN Server 2 running on http://localhost:8766");
+});
